4c3aaaa1eef6a42d70115b9c7964a3b9a88263f1
dev-OwaisKhan <dev.owaiskhan@gmail.com>
2023-03-01 16:16:27 +0530

    CHOICE ADD

--- Main.java
+++ Main.java
@@ -2,11 +2,14 @@ import java.util.*;
 class Main
 {
 
+    //  Static variables and Scanner object for input.
     static Scanner scan = new Scanner (System.in);
     static int main_choice;
 
+    // Method for main-menu choice input.
     public static void main_menu()
     {
+        // Try and catch for in case user enter a invalid character 
         try{
             
             System.out.println("--------------------Main Menu--------------------");
@@ -17,7 +20,7 @@ class Main
             System.out.println("               Enter 5: For Stack");
             System.out.println("               Enter 0: To Exit");
 
-            main_choice = scan.nextInt();
+            main_choice = Integer.parseInt(scan.nextLine().trim());
         }
         catch(Exception e)
         {
@@ -26,6 +29,8 @@ class Main
     }
 
     public static void main(String[] args) {
+        
+        // Initial 
         main_menu();
         switch (main_choice)
         {
@@ -33,29 +38,35 @@ class Main
             {
 
             } break;
+
             case 1:
             {
 
             } break;
+
             case 2:
             {
 
             } break;
+
             case 3:
             {
 
             } break;
+
             case 4:
             {
 
             } break;
+
             case 5:
             {
 
             } break;
+
             default:
             {
-                System.out.println("!!INVALID CHOICE, Enter a valid Number");
+                System.out.println("INVALID CHOICE!, ENTER A VALID NUMBER. \n");
                 main_menu();
             }
 
