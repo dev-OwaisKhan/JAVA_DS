5467cb4d475e30bdbc978cc387d3bd21e0534c7e
dev-OwaisKhan <dev.owaiskhan@gmail.com>
2023-03-31 16:03:52 +0530

    Meu_improvement

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
+/**
+ * DataStructure to add
+ * Queue
+ * Stack
+ * List - ArrayList, LinkedList
+ * Set - HashSet, Linked-HashSet, TreeSet.
+ * Map - HashMap, TreeMap
+ */
 import java.util.*;
 class Main
 {
@@ -6,6 +14,7 @@ class Main
     static Scanner scan = new Scanner (System.in);
     static int main_choice, size;
     static String datatype;
+    static String datastructure;
     static boolean condition = true;
     
 
@@ -18,12 +27,12 @@ class Main
             try{
                 
                 System.out.println("--------------------Main Menu--------------------");
-                System.out.println("               Enter 1: For Array");
-                System.out.println("               Enter 2: For Array-List");
-                System.out.println("               Enter 3: For Linked-List");
-                System.out.println("               Enter 4: For Queue");
-                System.out.println("               Enter 5: For Stack");
-                System.out.println("               Enter 0: To Exit");
+                System.out.println("\n\t\tEnter 1: For List");
+                System.out.println("\t\tEnter 2: For Map");
+                System.out.println("\t\tEnter 3: For Queue");
+                System.out.println("\t\tEnter 4: For Set");
+                System.out.println("\t\tEnter 5: For Stack");
+                System.out.println("\t\tEnter 0: To Exit");
 
                 main_choice = Integer.parseInt(scan.nextLine().trim());
                 break;
@@ -34,21 +43,28 @@ class Main
                 continue;
             }
         }
+        data_structure();
     }
 
     
     // Function to input of the type of DS and the size of the ds
 
-    public static void size_datatype(String ds)
+    public static void size_and_datatype()
     {
-       
+        boolean conti = false;
         while (true)
         {
-            System.out.println("Enter :\tS for String, I for Integer, D for Double\n\nChoose the Datatype of the "+ds);
+            System.out.println("Enter :\tS for String, I for Integer, D for Double, 0 To Go Back To Main Menu\n\nChoose the Datatype of the "+datastructure);
             datatype =  scan.nextLine().trim().toLowerCase();
             System.out.println(datatype);
             if (datatype.equals("s") || datatype.equals("i") || datatype.equals("d"))
             {
+                conti = true;
+                break;
+            }
+            else if(datatype.equals("0"))
+            {
+                main_menu();
                 break;
             }
             else{
@@ -56,9 +72,9 @@ class Main
                 continue;
             }
         }
-        while (true)
+        while (conti)
         {    
-            System.out.println("Enter the size of "+ds+" (1-20)");     
+            System.out.println("Enter the size of "+datastructure+" (1-20)");     
             try
             {
                 size = Integer.parseInt(scan.nextLine().trim());
@@ -79,16 +95,12 @@ class Main
         }
     }
 
-
-
-    public static void main(String[] args) {
-        
-        // Initial Start Menu 
-        main_menu();
+    public static void data_structure()
+    {
         while (condition)
         {
                 switch (main_choice)
-            {
+                {
                 case 0:
                 {
                     System.out.println("\n\t\t-----Exit-----");
@@ -97,30 +109,118 @@ class Main
 
                 case 1:
                 {
-                    size_datatype("Array");
-
+                    System.out.println("--------------------List--------------------");
+                    System.out.println("\n\t\tEnter 1: For ArrayList");
+                    System.out.println("\t\tEnter 2: For LinkedList");
+                    System.out.println("\t\tEnter 0: For Back To Main Menu");
+                    int choice = Integer.parseInt(scan.nextLine().trim());
+                    switch (choice)
+                    {
+                        case 0:
+                        {
+                            System.out.println("\t    ----Back To Main Menu----\n");
+                            main_menu();
+                        }break;
+                        case 1:
+                        {
+                            datastructure = "ArrayList";
+                            size_and_datatype();
+                        }break;
+                        case 2:
+                        {
+                            datastructure = "LinkedList";
+                            size_and_datatype();
+                        }break;
+                        default:
+                        {
+                            System.out.println("INVALID CHOICE!, ENTER A VALID NUMBER. \n"); 
+                        }
+                    }
                 } break;
 
                 case 2:
                 {
-                    size_datatype("Array-List");
+                    System.out.println("--------------------Map--------------------");
+                    System.out.println("\n\t\tEnter 1: For HashMap");
+                    System.out.println("\t\tEnter 2: For TreeMap");
+                    System.out.println("\t\tEnter 0: For Back To Main Menu");
+                    int choice = Integer.parseInt(scan.nextLine().trim());
+                    switch(choice)
+                    {
+                        case 0:
+                        {
+                            System.out.println("\t    ----Back To Main Menu----\n");
+                            main_menu();
+                        }break;
+                        case 1:
+                        {
+                            datastructure = "HaspMap";
+                            size_and_datatype();
+                        }break;
+                        case 2:
+                        {
+                            datastructure = "TreeMap";
+                            size_and_datatype();
+                        }break;
+                        default:
+                        {
+                            System.out.println("INVALID CHOICE!, ENTER A VALID NUMBER. \n"); 
+                        }
+                    }
+                    
                 } break;
 
                 case 3:
                 {
 
-                    size_datatype("Linked-List");
+                    System.out.println("--------------------Queue--------------------");
+                    datastructure = "Queue";
+                    size_and_datatype();
 
                 } break;
 
                 case 4:
                 {
-                    size_datatype("Queue");
+                    System.out.println("--------------------Set--------------------");
+                    System.out.println("\n\t\tEnter 1: For HashSet");
+                    System.out.println("\t\tEnter 2: For Treeset");
+                    System.out.println("\t\tEnter 3: For LinkedHashSet");
+                    System.out.println("\t\tEnter 0: For Back To Main Menu");   
+                    int choice = Integer.parseInt(scan.nextLine().trim());
+                    switch(choice)
+                    {
+                        case 0:
+                        {
+                            System.out.println("\t    ----Back To Main Menu----\n");
+                            main_menu();
+                        }break;
+                        case 1:
+                        {
+                            datastructure = "HashSet";
+                            size_and_datatype();
+                        }break;
+                        case 2:
+                        {
+                            datastructure = "TreeSet";
+                            size_and_datatype();
+                        }break;
+                        case 3:
+                        {
+                            datastructure = "LinkedHashSet";
+                            size_and_datatype();
+                        }break;
+                        default:
+                        {
+                            System.out.println("INVALID CHOICE!, ENTER A VALID NUMBER. \n"); 
+                        }
+                    }
                 } break;
 
                 case 5:
                 {
-                    size_datatype("Stack");
+                    System.out.println("--------------------Stack--------------------");
+                    datastructure = "Stack";
+                    size_and_datatype();
                 } break;
 
                 default:
@@ -132,7 +232,12 @@ class Main
             break;
 
         }
-        
+    }
 
+
+    public static void main(String[] args) {
+        
+        // Initial Start Menu 
+        main_menu();
     }
 }
